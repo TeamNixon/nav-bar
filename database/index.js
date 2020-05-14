16e7d9c1b5713d84353397f51c2527ebb37cd392
justfrankie <zhuangfrankie@gmail.com>
2020-05-14 14:53:40 -0700

    Merged Product Display module  as well as Reviews module

--- database/index.js
+++ database/index.js
@@ -4,16 +4,16 @@ var db = mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const Product = require('./schema.js');
-
+const database = require("./schema.js");
+// const Product = require('./schema.js');
 let controllers = {
-  getAll: () => Product.find({}),
+  getAll: () => database.Product.find({}),
   search: (query) =>
-    Product.find({ product_name: { $regex: query, $options: 'i' } }),
+  database.Product.find({ product_name: { $regex: query, $options: 'i' } }),
   // Product.find( {
   //     $or : [ {product_size: query }, {product_price: query} ]
   // })
-  getCartItem: () => Product.find({ in_cart: true }),
+  getCartItem: () => database.Product.find({ in_cart: true }),
 };
 
 module.exports = controllers;
--- database/schema.js
+++ database/schema.js
@@ -21,6 +21,49 @@ let productSchema = new Schema({
   images: [{type: String, }],
 });
 
+
+
 let Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+
+
+// OMAR'S PRODUCT MODEL ===========================================================================
+const productsSchemaR = new Schema({
+  productId: Number,
+  title: String,
+  images: [String],
+  reviews: [{type: Schema.Types.ObjectId, ref: 'Reviews'}]
+});
+
+const modelR = mongoose.model("productsR", productsSchemaR);
+
+// OMAR'S REVIEW MODEL ===========================================================================
+const reviewsSchema = new Schema({
+  rating: Number,
+  title: String,
+  body: String,
+  date: Date,
+  name: String,
+  style: {
+    classic: Boolean,
+    funky: Boolean,
+    daily_wear: Boolean,
+    sporty: Boolean
+  },
+  verified_purchase: Boolean,
+  product_id: {type: Schema.Types.ObjectId},
+  product_serial: String,
+  image: String,
+  likes: Number,
+  dislikes: Number
+});
+
+const modelRR = mongoose.model("Reviews", reviewsSchema);
+
+// module.exports = Product;
+// module.exports = model;
+module.exports = {
+  Product: Product,
+  modelR: modelR, // omar's product
+  modelRR: modelRR // omar's reviews
+};
